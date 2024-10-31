3b14d0bd82fc296f2877f4dc5ea5ce5270bbc4a8
SantiagoCarranza <santiagocarranzazinny@gmail.com>
2024-10-31 16:38:29 -0300

    map sin uso

--- Globals/globals.cpp
+++ Globals/globals.cpp
@@ -6,4 +6,3 @@ unordered_map<string, unordered_map<string, Estadisticas>> estadisticas;
 unordered_map<string, multimap<int, Partido *>> goles_por_competicion;
 unordered_map<string, int> goles_totales_por_competicion;
 vector<unique_ptr<Partido>> partidos;
-std::unordered_map<std::string, std::vector<Partido>> partidosPorCompeticion;
